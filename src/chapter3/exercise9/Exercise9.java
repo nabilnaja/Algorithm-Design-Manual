863de37b509992fe9d681e4d54e52894ba5275a9
nabilnaja <nabilnaja9@gmail.com>
2021-11-15 21:39:59 +0100

    add solutionto ch 3 exe 9

--- src/chapter3/exercise9/Exercise9.java
+++ src/chapter3/exercise9/Exercise9.java
@@ -1,24 +1,74 @@
 package chapter3.exercise9;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
 
 public class Exercise9 {
 
-    Map<Integer, String> phoneKey;
+    private static Map<Character, Integer> letterToPhoneKey;
+
+    static {
+        letterToPhoneKey = generateLetterPhoneKeyMapping();
+    }
 
     public Exercise9() {
-        phoneKey = Map.ofEntries(
-                new AbstractMap.SimpleEntry<>(2, "abc"),
-                new AbstractMap.SimpleEntry<>(3, "def"),
-                new AbstractMap.SimpleEntry<>(4, "ghi"),
-                new AbstractMap.SimpleEntry<>(5, "jkl"),
-                new AbstractMap.SimpleEntry<>(6, "mno"),
-                new AbstractMap.SimpleEntry<>(7, "pqrs"),
-                new AbstractMap.SimpleEntry<>(8, "tuv"),
-                new AbstractMap.SimpleEntry<>(9, "wxyz")
+
+    }
+
+    public static List<String> phoneKeyToWord(String phoneKey, List<String> words) {
+        final Map<String, List<String>> hashedWords = hashWords(words);
+        if (hashedWords.containsKey(phoneKey)) {
+            return hashedWords.get(phoneKey);
+        }
+        return new ArrayList<>();
+    }
+
+    private static Map<Character, Integer> generateLetterPhoneKeyMapping() {
+        return Map.ofEntries(
+                new AbstractMap.SimpleEntry<>('a', 2),
+                new AbstractMap.SimpleEntry<>('b', 2),
+                new AbstractMap.SimpleEntry<>('c', 2),
+                new AbstractMap.SimpleEntry<>('d', 3),
+                new AbstractMap.SimpleEntry<>('e', 3),
+                new AbstractMap.SimpleEntry<>('f', 3),
+                new AbstractMap.SimpleEntry<>('g', 4),
+                new AbstractMap.SimpleEntry<>('h', 4),
+                new AbstractMap.SimpleEntry<>('i', 4),
+                new AbstractMap.SimpleEntry<>('j', 5),
+                new AbstractMap.SimpleEntry<>('k', 5),
+                new AbstractMap.SimpleEntry<>('l', 5),
+                new AbstractMap.SimpleEntry<>('m', 6),
+                new AbstractMap.SimpleEntry<>('n', 6),
+                new AbstractMap.SimpleEntry<>('o', 6),
+                new AbstractMap.SimpleEntry<>('p', 7),
+                new AbstractMap.SimpleEntry<>('q', 7),
+                new AbstractMap.SimpleEntry<>('r', 7),
+                new AbstractMap.SimpleEntry<>('s', 7),
+                new AbstractMap.SimpleEntry<>('t', 8),
+                new AbstractMap.SimpleEntry<>('u', 8),
+                new AbstractMap.SimpleEntry<>('v', 8),
+                new AbstractMap.SimpleEntry<>('w', 9),
+                new AbstractMap.SimpleEntry<>('x', 9),
+                new AbstractMap.SimpleEntry<>('y', 9),
+                new AbstractMap.SimpleEntry<>('z', 9)
         );
+    }
+
+
+    private static Map<String, List<String>> hashWords(List<String> words) {
+        Map<String, List<String>> hashedWords = new HashMap<>();
+        words.forEach(s -> {
+            final String hash = hashWord(s, letterToPhoneKey);
+            hashedWords.computeIfAbsent(hash, k -> new ArrayList<>()).add(s);
+        });
+        return hashedWords;
+    }
+
+    private static String hashWord(String word, Map<Character, Integer> letterToPhoneKey) {
+        StringBuilder sb = new StringBuilder();
+        // the map should contain the character. we may add few test to check if the letter exist or if the word contains special char
+        word.chars().forEach(ch -> sb.append(letterToPhoneKey.get((char) ch)));
+        return sb.toString();
     }
 
 }
--- src/chapter3/exercise9/Exercise9Test.java
+++ src/chapter3/exercise9/Exercise9Test.java
@@ -1,7 +1,51 @@
 package chapter3.exercise9;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Exercise9Test {
 
+    static List<List<String>> expectedResult;
+    static List<String> words;
+    static List<String> queries;
+
+    @BeforeAll
+    void setUp() {
+        expectedResult = new ArrayList<>();
+        queries = new ArrayList<>();
+        words = readWordsFromFile("src/chapter3/exercise9/words_alpha.txt");
+        queries.add("269");
+        expectedResult.add(Arrays.asList("amy", "any", "boy", "bow", "box", "coy", "cow", "cox", "coz"));
+    }
+
+    @Test
+    public void words() {
+        List<String> result = Exercise9.phoneKeyToWord(queries.get(0), words);
+        assertEquals(expectedResult.get(0).size(), result.size());
+        IntStream.range(0, result.size()).forEach(i -> assertEquals(expectedResult.get(0).get(i), result.get(i)));
+    }
+
+
+    private static List<String> readWordsFromFile(String filename) {
+        List<String> words;
+        try {
+            words = Files.readAllLines(Paths.get(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+        return words;
+    }
 }
