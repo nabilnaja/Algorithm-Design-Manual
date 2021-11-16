8897a5180ab192e2900017c8e4f7dbb3d0970053
nabilnaja <nabilnaja9@gmail.com>
2021-11-16 19:45:21 +0100

    Add solution to ch3 exe10

--- src/chapter3/exercise10/Exercise10.java
+++ src/chapter3/exercise10/Exercise10.java
@@ -2,4 +2,21 @@ package chapter3.exercise10;
 
 public class Exercise10 {
 
+    public static boolean areAnagrams(String w1, String w2) {
+        if (w1.length() != w2.length()) {
+            return false;
+        }
+        int[] letterCount = new int[26];
+
+        for (int i = 0; i < w1.length(); i++) {
+            letterCount[w1.charAt(i) - 97]++;
+            letterCount[w2.charAt(i) - 97]--;
+        }
+        for (int i : letterCount) {
+            if (i != 0)
+                return false;
+        }
+        return true;
+    }
+
 }
