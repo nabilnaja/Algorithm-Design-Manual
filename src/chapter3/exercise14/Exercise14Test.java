4366ba26fd0a72e55354dcfe1d369e815cab538a
nabilnaja <nabilnaja9@gmail.com>
2021-11-22 22:05:45 +0100

    Add test class for ch3 ex14.

---
+++ src/chapter3/exercise14/Exercise14Test.java
@@ -0,0 +1,13 @@
+package chapter3.exercise14;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Exercise14Test {
+
+    @BeforeAll
+    void setUp() {}
+
+}
