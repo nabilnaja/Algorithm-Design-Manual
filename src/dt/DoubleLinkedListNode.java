bc58f4e91d52fae48f30912371ca21179f6cf327
nabilnaja <nabilnaja9@gmail.com>
2021-11-23 21:10:13 +0100

    Add double linked list node

---
+++ src/dt/DoubleLinkedListNode.java
@@ -0,0 +1,13 @@
+package dt;
+
+public class DoubleLinkedListNode {
+    public int val;
+    public DoubleLinkedListNode prev;
+    public DoubleLinkedListNode next;
+
+    public DoubleLinkedListNode(int val, DoubleLinkedListNode prev, DoubleLinkedListNode next) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+}
