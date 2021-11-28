14a6ec9aada832b598a8fedc8bc0c0786ebbe945
nabilnaja <nabilnaja9@gmail.com>
2021-11-28 19:32:05 +0100

    Add solution to ch3 ex21

---
+++ src/chapter3/exercise21/Exercise21.java
@@ -0,0 +1,31 @@
+package chapter3.exercise21;
+
+import dt.TreeNode;
+
+public class Exercise21 {
+
+
+    public static TreeNode concat(TreeNode root1, TreeNode root2){
+        if (root1 == null && root2 == null)
+            return null;
+        if (root1 == null)
+            return root2;
+        if (root2 == null)
+            return root1;
+
+        TreeNode min = findMin(root2, null);
+        min.left = root1;
+        min.right = root2;
+        return min;
+    }
+
+    private static TreeNode findMin(TreeNode root, TreeNode prev){
+        if (root.left == null)
+        {
+            if (prev != null)
+                prev.left = null;
+            return root;
+        }
+        return findMin(root.left, root);
+    }
+}
