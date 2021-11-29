ae6bb489c5502a9f923601eb12df324ac6ada6ec
nabilnaja <nabilnaja9@gmail.com>
2021-11-29 20:17:41 +0100

    Add solution to ch3 exe22

---
+++ src/chapter3/exercise22/Exercise22.java
@@ -0,0 +1,34 @@
+package chapter3.exercise22;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Exercise22 {
+    private Queue<Integer> minHeap, maxHeap;
+
+    Exercise22() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    void add(int num) {
+        if (minHeap.size() == maxHeap.size()) {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        } else {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    double getMedian() {
+        int median;
+        if (minHeap.size() > maxHeap.size()) {
+            median = minHeap.peek();
+        } else {
+            median = (minHeap.peek() + maxHeap.peek()) / 2;
+        }
+        return median;
+    }
+}
